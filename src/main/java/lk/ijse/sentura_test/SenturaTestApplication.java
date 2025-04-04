3de5fdd3c64b75887b1664abfcea86e85eb7ff86
kavindumal <kavindu11250403@gmail.com>
2025-04-04 09:04:43 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/sentura_test/SenturaTestApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.sentura_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenturaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/sentura_test/SenturaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.sentura_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenturaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
