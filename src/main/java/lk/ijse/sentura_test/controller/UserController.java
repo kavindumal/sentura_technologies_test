3e732a3c3a544d8692fb43a2f2cc05b5d779f991
kavindumal <kavindu11250403@gmail.com>
2025-04-04 09:37:17 +0530

    Added UserService and UserController classes for user management functionality.

--- src/main/java/lk/ijse/sentura_test/controller/UserController.java
+++ src/main/java/lk/ijse/sentura_test/controller/UserController.java
@@ -1,4 +1,41 @@
 package lk.ijse.sentura_test.controller;
 
+import lk.ijse.sentura_test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody String user) throws IOException {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<String> listUsers() throws IOException {
+        return ResponseEntity.ok(userService.listUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getUser(@PathVariable String id) throws IOException {
+        return ResponseEntity.ok(userService.getUser(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody String user) throws IOException {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) throws IOException {
+        return ResponseEntity.ok(userService.deleteUser(id));
+    }
 }
--- src/main/java/lk/ijse/sentura_test/service/UserService.java
+++ src/main/java/lk/ijse/sentura_test/service/UserService.java
@@ -1,4 +1,37 @@
 package lk.ijse.sentura_test.service;
 
+import lk.ijse.sentura_test.config.WeavyConfig;
+import lk.ijse.sentura_test.util.HttpUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private HttpUtil httpUtil;
+
+    @Autowired
+    private WeavyConfig weavyConfig;
+
+    public String createUser(String userJson) throws IOException {
+        return httpUtil.post(weavyConfig.getWeavyApiUrl(), userJson, weavyConfig.getWeavyApiToken());
+    }
+
+    public String listUsers() throws IOException {
+        return httpUtil.get(weavyConfig.getWeavyApiUrl(), weavyConfig.getWeavyApiToken());
+    }
+
+    public String getUser(String userId) throws IOException {
+        return httpUtil.get(weavyConfig.getWeavyApiUrl() + "/" + userId, weavyConfig.getWeavyApiToken());
+    }
+
+    public String updateUser(String userId, String userJson) throws IOException {
+        return httpUtil.put(weavyConfig.getWeavyApiUrl() + "/" + userId, userJson, weavyConfig.getWeavyApiToken());
+    }
+
+    public String deleteUser(String userId) throws IOException {
+        return httpUtil.delete(weavyConfig.getWeavyApiUrl() + "/" + userId, weavyConfig.getWeavyApiToken());
+    }
 }
